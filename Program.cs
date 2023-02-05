ffda7b79a7381606b3321abd9bd9ad386c7f7695
linas <linas@DESKTOP-AGOFPS7>
2023-02-05 22:53:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Create a user Login System, where the user can first register and then Login in. 
+            The Program should check if the user has entered the correct username and password, 
+            when login in (so the same ones that he used when registering).
+            As we haven't covered storing data yet, just create the program in a way, 
+            that registering and logging in, happen in the same execution of it.
+            User If statements and User Input and Methods to solve the challenge.
+            */
+            //Code Exercise
+            Console.WriteLine("Please enter the new registers name:\n");
+        }
+    }
+}
